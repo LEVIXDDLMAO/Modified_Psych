568344a4c74d3906f10891882bab4bfe808b96d0
LeviXD <92174516+LEVIXDDLMAO@users.noreply.github.com>
2022-09-22 10:38:01 -0600

    forgot

--- source/options/OptionsState.hx
+++ source/options/OptionsState.hx
@@ -49,7 +49,7 @@ class OptionsState extends MusicBeatState
 			case 'Adjust Delay and Combo':
 				LoadingState.loadAndSwitchState(new options.NoteOffsetState());
 			case 'Osu!Maina Options':
-			    openSubState(new options.Osuoptions());
+			    openSubState(new options.Osu!options());
 			    
 		}
 	}
