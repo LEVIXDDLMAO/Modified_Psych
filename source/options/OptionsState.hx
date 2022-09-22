b29848c604c441570d65926036ecfa65eb68f99f
LeviXD <92174516+LEVIXDDLMAO@users.noreply.github.com>
2022-09-22 11:35:18 -0600

    there

--- source/options/OptionsState.hx
+++ source/options/OptionsState.hx
@@ -29,7 +29,7 @@ using StringTools;
 
 class OptionsState extends MusicBeatState
 {
-	var options:Array<String> = ['Controls','Graphics', 'Visuals and UI', 'Gameplay', 'Osu!Maina Things'];
+	var options:Array<String> = ['Controls','Graphics', 'Visuals and UI', 'Gameplay', 'Osu!Maina'];
 	private var grpOptions:FlxTypedGroup<Alphabet>;
 	private static var curSelected:Int = 0;
 	public static var menuBG:FlxSprite;
@@ -44,7 +44,7 @@ class OptionsState extends MusicBeatState
 				openSubState(new options.VisualsUISubState());
 			case 'Gameplay':
 				openSubState(new options.GameplaySettingsSubState());
-			case 'Osu!Maina Things':
+			case 'Osu!Maina':
 				openSubState(new options.Osu!Maina());
 			
 			    
