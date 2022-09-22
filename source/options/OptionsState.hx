3a9945fa2906a9ce2a68f7697bb9146823e93991
LeviXD <92174516+LEVIXDDLMAO@users.noreply.github.com>
2022-09-22 13:42:48 -0600

    testing

--- source/options/OptionsState.hx
+++ source/options/OptionsState.hx
@@ -29,15 +29,13 @@ using StringTools;
 
 class OptionsState extends MusicBeatState
 {
-	var options:Array<String> = ['Note Colors', 'Controls', 'Adjust Delay and Combo', 'Graphics', 'Visuals and UI', 'Gameplay'];
+	var options:Array<String> = [ 'Controls', 'Osu', 'Graphics', 'Visuals and UI', 'Gameplay'];
 	private var grpOptions:FlxTypedGroup<Alphabet>;
 	private static var curSelected:Int = 0;
 	public static var menuBG:FlxSprite;
 
 	function openSelectedSubstate(label:String) {
 		switch(label) {
-			case 'Note Colors':
-				openSubState(new options.NotesSubState());
 			case 'Controls':
 				openSubState(new options.ControlsSubState());
 			case 'Graphics':
@@ -46,8 +44,8 @@ class OptionsState extends MusicBeatState
 				openSubState(new options.VisualsUISubState());
 			case 'Gameplay':
 				openSubState(new options.GameplaySettingsSubState());
-			case 'Adjust Delay and Combo':
-				LoadingState.loadAndSwitchState(new options.NoteOffsetState());
+			case 'Osu':
+				LoadingState.loadAndSwitchState(new options.Osu!MainaSubState());
 		}
 	}
 
